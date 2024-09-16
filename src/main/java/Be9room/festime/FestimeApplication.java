8ed4546eb1c9deec3386a0b8f935106bbbebe9c6
thsghdud13 <thsgdud25@naver.com>
2024-09-16 20:46:48 +0900

    feat : first commit

---
+++ src/main/java/Be9room/festime/FestimeApplication.java
@@ -0,0 +1,13 @@
+package Be9room.festime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Be9room/festime/FestimeApplicationTests.java
@@ -0,0 +1,13 @@
+package Be9room.festime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
