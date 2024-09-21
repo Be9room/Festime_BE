691e51ced3e9263b2662ddf466aaa560c5a38b45
thsghdud13 <thsgdud25@naver.com>
2024-09-21 14:39:58 +0900

    Feat : HealthCheck 기능 추가

---
+++ src/main/java/Be9room/festime/controller/RootController.java
@@ -0,0 +1,13 @@
+package Be9room.festime.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "OK";
+    }
+}
