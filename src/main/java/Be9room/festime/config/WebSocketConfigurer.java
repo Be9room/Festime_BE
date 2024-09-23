47af74a1bfa968a26bc9e2b31182dca924d04cdd
thsghdud13 <thsgdud25@naver.com>
2024-09-23 12:18:57 +0900

    Add : WebsocketCofigurer 생성

--- src/main/java/Be9room/festime/config/WebSocketConfigurer.java
+++ src/main/java/Be9room/festime/config/WebSocketConfigurer.java
@@ -1,33 +1,32 @@
-package submeet.backend.config;
+package Be9room.festime.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import submeet.backend.handler.StompHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
-    private final StompHandler stompHandler;
+//    private final StompHandler stompHandler;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp/chat")
+        registry.addEndpoint("/stomp/guestbook")
                 .setAllowedOrigins("*");
 //                .withSockJS();
-        //주소 : ws://localhost:8080/stomp/chat
+        //주소 : ws://localhost:8080/stomp/guestbook
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setPathMatcher(new AntPathMatcher("."));
+//        registry.setPathMatcher(new AntPathMatcher("."));
         registry.setApplicationDestinationPrefixes("/pub");
-        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+//        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+        registry.enableSimpleBroker("/sub");
     }
 
 //    @Override
