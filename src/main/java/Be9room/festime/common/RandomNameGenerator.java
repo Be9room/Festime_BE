978bcd040da201bc5805fbbbccb8c74cae0c5f68
thsghdud13 <thsgdud25@naver.com>
2024-09-26 01:59:41 +0900

    feat : 이름 자동으로 지어주는 클래스 구현

--- src/main/java/Be9room/festime/common/RandomNameGenerator.java
+++ src/main/java/Be9room/festime/common/RandomNameGenerator.java
@@ -1,2 +1,24 @@
-package Be9room.festime.common;public class NameGeneratorFactory {
+package Be9room.festime.common;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class RandomNameGenerator {
+    List<String> adjective = Arrays.asList(
+        "노래하는", "졸린", "배고픈", "목마른", "피곤한", "행복한", "신나는", "화려한", "유쾌한" ,
+            "기대하는", "반짝이는", "특별한", "감동하는", "멋진", "춤추는", "대단한"
+    );
+
+    List<String> objective = Arrays.asList(
+            "인덕이", "안뇽이"
+    );
+    public String generate(){
+        Collections.shuffle(adjective);
+        Collections.shuffle(objective);
+        return adjective.get(0) + " " + objective.get(0);
+    }
 }
