b44e6bc8aba121b4490ea9d0346e33a33051002c
thsghdud13 <thsgdud25@naver.com>
2024-09-26 01:59:59 +0900

    feat : 이름 발급하는 라우터 생성

--- src/main/java/Be9room/festime/dto/MemberDto.java
+++ src/main/java/Be9room/festime/dto/MemberDto.java
@@ -1,2 +1,17 @@
-package Be9room.festime.dto;public class MemberDto {
+package Be9room.festime.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MemberDto {
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    public static class MemberResponseDto {
+        String memberName;
+        String memberId;
+    }
 }
