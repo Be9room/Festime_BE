138fa92d9a8fb713f3978bd7c204c2f950741716
thsghdud13 <thsgdud25@naver.com>
2024-09-26 15:59:27 +0900

    feat : 메시지 저장기능 구현

--- src/main/java/Be9room/festime/controller/MessageController.java
+++ src/main/java/Be9room/festime/controller/MessageController.java
@@ -2,6 +2,7 @@ package Be9room.festime.controller;
 
 import Be9room.festime.dto.MessageDto;
 import Be9room.festime.enums.MessageType;
+import Be9room.festime.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -13,17 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class MessageController {
     private final SimpMessagingTemplate template;
+    private final MessageService messageService;
     /**
      * 채팅 입장
      * /pub/chat/enter 로 보내면 여기로 온다.
      * @param message
      */
     @MessageMapping(value = "/guestbook/enter")
+    @Deprecated
     public void enter(MessageDto message){
         message.setMessageType(MessageType.ENTER.toString());
         message.setMessage(message.getMemberName() + "님이 방명록에 참여했어요!");
 
-        /* 메세지 저장 부분 */
+        messageService.save(message);
 
         template.convertAndSend("/topic/guestbook", message);
     }
@@ -36,7 +39,7 @@ public class MessageController {
     public void message(MessageDto message){
         message.setMessageType(MessageType.MESSAGE.toString());
 
-        /* 메세지 저장 부분 */
+        messageService.save(message);
 
         template.convertAndSend("/topic/guestbook", message);
     }
@@ -46,11 +49,12 @@ public class MessageController {
      * @param message
      */
     @MessageMapping(value = "/guestbook/leave")
+    @Deprecated
     public void leave(MessageDto message){
         message.setMessageType(MessageType.LEAVE.toString());
         message.setMessage(message.getMemberName() + "님이 방명록에서 나가셨어요!");
 
-        /* 메세지 저장 부분 */
+        messageService.save(message);
 
         template.convertAndSend("/topic/guestbook",  message);
     }
